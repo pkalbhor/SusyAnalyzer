4559ebf61b7360ae046620af4c0d7633f4bab7db
Pritam <physics.pritam@cern.ch>
2018-06-16 00:57:01 +0200

    2nd commit. Just added i/p root file and disable few lines which are not suppose to work. This code should run successfully.Basic working code. Starting point for complicated analyzer.

--- plugins/SusyAnalyzer.cc
+++ plugins/SusyAnalyzer.cc
@@ -57,7 +57,7 @@ class SusyAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
       virtual void endJob() override;
 
       // ----------member data ---------------------------
-      edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
+      //edm::EDGetTokenT<TrackCollection> tracksToken_;  //used to select what tracks to read from configuration file
 };
 
 //
@@ -72,8 +72,7 @@ class SusyAnalyzer : public edm::one::EDAnalyzer<edm::one::SharedResources>  {
 // constructors and destructor
 //
 SusyAnalyzer::SusyAnalyzer(const edm::ParameterSet& iConfig)
- :
-  tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks")))
+//  tracksToken_(consumes<TrackCollection>(iConfig.getUntrackedParameter<edm::InputTag>("tracks")))
 
 {
    //now do what ever initialization is needed
@@ -99,7 +98,7 @@ void
 SusyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
 {
    using namespace edm;
-
+/*
     Handle<TrackCollection> tracks;
     iEvent.getByToken(tracksToken_, tracks);
     for(TrackCollection::const_iterator itTrack = tracks->begin();
@@ -108,7 +107,7 @@ SusyAnalyzer::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup)
       // do something with track parameters, e.g, plot the charge.
       // int charge = itTrack->charge();
     }
-
+*/
 #ifdef THIS_IS_AN_EVENT_EXAMPLE
    Handle<ExampleData> pIn;
    iEvent.getByLabel("example",pIn);
--- python/ConfFile_cfg.py
+++ python/ConfFile_cfg.py
@@ -4,12 +4,12 @@ process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(10) )
 
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
-        'file:myfile.root'
+        'root://eoscms.cern.ch//eos/cms/store/mc/RunIISummer16MiniAODv2/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/MINIAODSIM/PUMoriond17_80X_mcRun2_asymptotic_2016_TrancheIV_v6_ext1-v2/80000/F283191C-11C4-E611-973D-00215E2EB74E.root'
     )
 )
 
